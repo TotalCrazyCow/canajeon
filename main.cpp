d6e3336288d825515db173d3588640c02dc116b0
TotalCrazyCow <m.abatangelo@gmail.com>
2024-07-03 23:33:12 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+const int DIMENSION = 2;
+const int GRID_SIZE = 101;
+
+struct Grid {
+    double x[GRID_SIZE];
+    double y[GRID_SIZE];
+};
+
+int main() {
+
+    Grid domain;
+    double x_min = 0;
+    double x_max = 1;
+    double y_min = 0;
+    double y_max = 1;
+
+    double step = (x_max - x_min)/(GRID_SIZE-1);
+
+    for (int i = 0; i < GRID_SIZE; i++) {
+        domain.x[i] = x_min + i*step;
+        domain.y[i] = y_min + i*step;
+    }
+
+
+    std::cout << "ciaooo\n";
+
+    return 0;
+}
